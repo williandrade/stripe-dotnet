05123c465ffcb54d9dc61020b8c92379045ef4fb
Olivier Bellone <ob@stripe.com>
2019-08-24 00:16:36 -0700

    Fix CA1040

--- src/Stripe.net/Services/_interfaces/INestedOptions.cs
+++ src/Stripe.net/Services/_interfaces/INestedOptions.cs
@@ -1,5 +1,8 @@
 namespace Stripe
 {
+    using System.Diagnostics.CodeAnalysis;
+
+    [SuppressMessage("Microsoft.Design", "CA1040", Justification = "Used for encoding parameters")]
     public interface INestedOptions
     {
         // this interface just needs to be implemented by any class that is
